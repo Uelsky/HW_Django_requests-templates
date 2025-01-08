3697f343da608f27a24791070fa947c42ea9b13d
Uelsky <maktavish123@gmail.com>
2025-01-08 17:12:37 +0300

    initial commit

---
+++ calculator/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render
+
+
+DATA = {
+        'omlet': {
+            'яйца, шт': 2,
+            'молоко, л': 0.1,
+            'соль, ч.л.': 0.5,
+        },
+        'pasta': {
+            'макароны, г': 0.3,
+            'сыр, г': 0.05,
+        },
+        'buter': {
+            'хлеб, ломтик': 1,
+            'колбаса, ломтик': 1,
+            'сыр, ломтик': 1,
+            'помидор, ломтик': 1,
+        },
+    }
+
+# Напишите ваш обработчик. Используйте DATA как источник данных
+# Результат - render(request, 'calculator/index.html', context)
+# В качестве контекста должен быть передан словарь с рецептом:
+# context = {
+#   'recipe': {
+#     'ингредиент1': количество1,
+#     'ингредиент2': количество2,
+#   }
+# }
+
+def handler(request, dish, servings=1):
+    recipe = {}
+    if servings > 0 and dish in DATA:
+        for i in DATA[dish]:
+            recipe.update(
+                {i: round(DATA[dish][i]*servings, 2)}
+            )
+    return render(
+        request,
+        'calculator/index.html',
+        context={'recipe': recipe}
+    )
